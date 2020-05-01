425ce44cccb682994eb75723f1e1d9fd4e3790d3
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-04-30 19:59:57 -0700

    Cleaning up voxels

--- deepchem/utils/voxel_utils.py
+++ deepchem/utils/voxel_utils.py
@@ -5,7 +5,7 @@ import logging
 import numpy as np
 logger = logging.getLogger(__name__)
 
-def convert_atom_to_voxel(molecule_xyz,
+def convert_atom_to_voxel(coordinates,
                           atom_index,
                           box_width,
                           voxel_width):
@@ -17,7 +17,7 @@ def convert_atom_to_voxel(molecule_xyz,
 
   Parameters:
   -----------
-  molecule_xyz: np.ndarray
+  coordinates: np.ndarray
     Array with coordinates of all atoms in the molecule, shape
     (N, 3).
   atom_index: int
@@ -26,34 +26,56 @@ def convert_atom_to_voxel(molecule_xyz,
     Size of the box in Angstroms.
   voxel_width: float
     Size of a voxel in Angstroms
+
+  Returns
+  -------
+  A list containing a numpy array of length 3 with `[i, j, k]`, the
+  voxel coordinates of specified atom.
   """
 
   indices = np.floor(
-      (molecule_xyz[atom_index] + box_width / 2.0) / voxel_width).astype(int)
+      (coordinates[atom_index] + box_width / 2.0) / voxel_width).astype(int)
   if ((indices < 0) | (indices >= box_width / voxel_width)).any():
     logger.warning(
          'Coordinates are outside of the box (atom id = %s,'
          ' coords xyz = %s, coords in box = %s' %
-         (atom_index, molecule_xyz[atom_index], indices))
+         (atom_index, coordinates[atom_index], indices))
 
   return ([indices])
 
-def convert_atom_pair_to_voxel(molecule_xyz_tuple, atom_index_pair, box_width,
+def convert_atom_pair_to_voxel(coordinates_tuple, atom_index_pair, box_width,
                                voxel_width):
-  """Converts a pair of atoms to a list of i,j,k tuples."""
+  """Converts a pair of atoms to a list of i,j,k tuples.
+
+
+  Parameters
+  ----------
+  coordinates_tuple: tuple
+    A tuple containing two molecular coordinate arrays of shapes `(N, 3)` and `(M, 3)`.
+  atom_index_pair: tuple
+    A tuple of indices for the atoms in the two molecules.
+  box_width: float
+    Size of the box in Angstroms.
+  voxel_width: float
+    Size of a voxel in Angstroms
+
+  Returns
+  -------
+  A list containing two numpy array of length 3 with `[i, j, k]`, the
+  voxel coordinates of specified atom.
+  """
 
   indices_list = []
   indices_list.append(
-      convert_atom_to_voxel(molecule_xyz_tuple[0], atom_index_pair[0],
+      convert_atom_to_voxel(coordinates_tuple[0], atom_index_pair[0],
                             box_width, voxel_width)[0])
   indices_list.append(
-      convert_atom_to_voxel(molecule_xyz_tuple[1], atom_index_pair[1],
+      convert_atom_to_voxel(coordinates_tuple[1], atom_index_pair[1],
                             box_width, voxel_width)[0])
   return (indices_list)
 
 
 def voxelize(get_voxels,
-             voxels_per_edge,
              box_width,
              voxel_width,
              hash_function,
@@ -72,8 +94,6 @@ def voxelize(get_voxels,
   ----------
   get_voxels: function
     Function that voxelizes inputs
-  voxels_per_edge: int
-    Number of voxels per one edge of box to voxelize.
   box_width: float, optional (default 16.0)
     Size of a box in which voxel features are calculated. Box
     is centered on a ligand centroid.
@@ -98,6 +118,8 @@ def voxelize(get_voxels,
   Tensor of shape (voxels_per_edge, voxels_per_edge,
   voxels_per_edge, nb_channel),
   """
+  # Number of voxels per one edge of box to voxelize.
+  voxels_per_edge = int(box_width/voxel_width)
   if dtype == "np.int8":
     feature_tensor = np.zeros(
         (voxels_per_edge, voxels_per_edge, voxels_per_edge,
